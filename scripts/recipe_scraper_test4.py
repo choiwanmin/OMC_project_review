404de9ce9ab5714f03152d1f43fca5c5b2cdbe78
lks <chgml9326@hanyang.ac.kr>
2022-12-21 18:30:28 +0900

    feat: json 파일 내보내기

--- scripts/recipe_scraper_test4.py
+++ scripts/recipe_scraper_test4.py
@@ -1,61 +1,64 @@
 import requests
 from bs4 import BeautifulSoup
 from omc.models import Recipe,CategoryT, CategoryS, CategoryI, CategoryM
+import json
+file_path = 'C:\recipe_pjt'
 
-# url = 'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1=&cat2=&cat3=&cat4=&fct=&order=reco&lastcate=cat4&dsearch=&copyshot=&scrap=&degree=&portion=&time=&niresource='
-# res = requests.get(url)
-# soup = BeautifulSoup(res.text, 'html.parser')
-# items = soup.select_one('#id_search_category table tbody tr td div.rcp_cate.st3')
-# items = soup.select_one('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a')
 def run():
-    # 해당 카테고리에 있는 레시피의 Id
-    # recipe_link = items.get('href').strip()
-    # recipe_link = recipe_link.split('/')[-1]
-    # print(recipe_link)
-    # Db에 저장되어 있는 mangaeId
     categoryT = CategoryT.objects.all()
     categoryS = CategoryS.objects.all()
     categoryI = CategoryI.objects.all()
     categoryM = CategoryM.objects.all()
+    data = {}
+    data['update_keys'] = []
     for catT in categoryT:
         for catS in categoryS:
             for catI in categoryI:
                 for catM in categoryM:
-                    url = f'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&fct=&order=reco&lastcate=cat4&dsearch=&copyshot=&scrap=&degree=&portion=&time=&niresource='
+                    url = f'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&order=date&page=1'
                     res = requests.get(url)
                     soup = BeautifulSoup(res.text, 'html.parser')
-                    
-                    url_lst = []
+                    page = 1
+
                     while soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a'):
-                        url_lst.append(url)
-                        url = f'https://www.10000recipe.com/recipe/list.html?cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&order=reco&page={len(url_lst)+1}'
+                        url = f'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&order=date&page={page}'
                         res = requests.get(url)
                         soup = BeautifulSoup(res.text, 'html.parser')
-
-                    for url in url_lst:
-                        url = url
-                        res = requests.get(url)
-                        soup = BeautifulSoup(res.text, 'html.parser')
-                        if soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a'):
-                            items = soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a')
-                            for item in items:
-                                recipe_link = item.get('href').strip()
-                                recipe_link = recipe_link.split('/')[-1]
-
-                                recipe = Recipe.objects.filter(mangaeId__iexact=recipe_link)
-                                if recipe:
-                                    recipe.update(categoryTId=catT,categorySId=catS, categoryIId=catI, categoryMId=catM)
-                                    
-                        else:
-                            print('검색결과가 없습니다.')
+                        items = soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a')
+                        idx = 0
+                        for item in items:
+                            recipe_link = item.get('href').strip()
+                            recipe_link = recipe_link.split('/')[-1]
+                            recipe = Recipe.objects.filter(mangaeId__iexact=recipe_link)
+                            # print(recipe_link)
+                            # print(Recipe.objects.get(mangaeId='6843136').name)
+                            data['update_keys'].append(
+                            {
+                                'recipe_pk' : Recipe.objects.all()[idx].pk,
+                                'categoryTId' : catT.pk,
+                                'categorySId' : catS.pk,
+                                'categoryIId' : catI.pk,
+                                'categoryMId' : catM.pk,
+                            })
+                            print(data)
+                            idx+=1
+                            if idx == 2:
+                                break
+                            # if recipe:
+                            #     recipe.update(categoryTId=catT,categorySId=catS, categoryIId=catI, categoryMId=catM)
+                        # page += 1
+                        # else:
+                        #     print('검색결과가 없습니다.')
+                        break
                     break
-    #           print(f'category I : {catI.index} 종료')
+                print(f'category I : {catI.index} 종료')
                 break
     #       print(f'category S : {catS.index} 종료')
             break
+        # with open(file_path, 'w') as outfile:
+        #     json.dump(data, outfile)
     #   print(f'category T : {catT.index} 종료')
         break
-
 
 
 
