ecce8671f43e4d3dbc95eee283c227fa6bd5b3c3
chomu97 <ach1219@naver.com>
2022-12-21 15:54:47 +0900

    refactor: category index type 변경, admin str 추가

--- omc/models.py
+++ omc/models.py
@@ -2,23 +2,35 @@ from django.db import models
 
 # Create your models here.
 class CategoryT(models.Model):
-    index = models.IntegerField()
+    index = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'cat T || {self.name}'
 
 
 class CategoryS(models.Model):
-    index = models.IntegerField()
+    index = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'cat S || {self.name}'
 
 
 class CategoryI(models.Model):
-    index = models.IntegerField()
+    index = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'cat I || {self.name}'
 
 
 class CategoryM(models.Model):
-    index = models.IntegerField()
+    index = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return f'catM || {self.name}'
 
 
 class Recipe(models.Model):
@@ -38,12 +50,18 @@ class Recipe(models.Model):
     categoryIId = models.ForeignKey(CategoryI, on_delete=models.SET_NULL, null=True, blank=True)
     categoryMId = models.ForeignKey(CategoryM, on_delete=models.SET_NULL, null=True, blank=True)
 
+    def __str__(self):
+        return f'recipe || {self.name}'
+
 
 class Ingredient(models.Model):
     type = models.CharField(max_length=30)
     name = models.CharField(max_length=30)
     volume = models.CharField(max_length=30)
     recipeId = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'ingredient || {self.type} || {self.name}'
 
 
 class RecipeOrder(models.Model):
@@ -52,9 +70,14 @@ class RecipeOrder(models.Model):
     thumbnail = models.URLField(null=True, blank=True)
     recipeId = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'order{self.recipeId}-{self.number} || {self.description}'
+
 
 class RecipeHashTag(models.Model):
     description = models.CharField(max_length=30)
     recipeId = models.ForeignKey(Recipe, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return f'hashTag || {self.recipeId}-{self.description}'
 
