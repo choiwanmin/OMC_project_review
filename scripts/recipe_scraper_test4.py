bb3776cd9cecbae0dec985210237269823902ebd
chomu97 <ach1219@naver.com>
2022-12-22 01:40:02 +0900

    fix: 연결 끊기지 않도록 수정

--- scripts/recipe_scraper_test4.py
+++ scripts/recipe_scraper_test4.py
@@ -2,63 +2,66 @@ import requests
 from bs4 import BeautifulSoup
 from omc.models import Recipe,CategoryT, CategoryS, CategoryI, CategoryM
 import json
-file_path = 'C:\recipe_pjt'
+import os
+from user_agent import generate_user_agent
+import time
+
 
 def run():
     categoryT = CategoryT.objects.all()
     categoryS = CategoryS.objects.all()
     categoryI = CategoryI.objects.all()
     categoryM = CategoryM.objects.all()
-    data = {}
-    data['update_keys'] = []
     for catT in categoryT:
+        data = {}
+        data['update_keys'] = []
         for catS in categoryS:
             for catI in categoryI:
+                if catT.pk ==1 and catS.pk == 1 and catI.pk <= 4:
+                    continue
                 for catM in categoryM:
                     url = f'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&order=date&page=1'
-                    res = requests.get(url)
+                    res = requests.get(url, timeout=3)
                     soup = BeautifulSoup(res.text, 'html.parser')
+                    items = soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a')
                     page = 1
-
-                    while soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a'):
-                        url = f'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&order=date&page={page}'
-                        res = requests.get(url)
-                        soup = BeautifulSoup(res.text, 'html.parser')
-                        items = soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a')
-                        idx = 0
-                        for item in items:
-                            recipe_link = item.get('href').strip()
-                            recipe_link = recipe_link.split('/')[-1]
-                            recipe = Recipe.objects.filter(mangaeId__iexact=recipe_link)
-                            # print(recipe_link)
-                            # print(Recipe.objects.get(mangaeId='6843136').name)
-                            data['update_keys'].append(
-                            {
-                                'recipe_pk' : Recipe.objects.all()[idx].pk,
-                                'categoryTId' : catT.pk,
-                                'categorySId' : catS.pk,
-                                'categoryIId' : catI.pk,
-                                'categoryMId' : catM.pk,
-                            })
-                            print(data)
-                            idx+=1
-                            if idx == 2:
-                                break
-                            # if recipe:
-                            #     recipe.update(categoryTId=catT,categorySId=catS, categoryIId=catI, categoryMId=catM)
-                        # page += 1
-                        # else:
-                        #     print('검색결과가 없습니다.')
-                        break
-                    break
+                    while len(soup.select('#contents_area_full ul div.result_none')) == 0:
+                        try:
+                            if page >= 2:
+                                headers = {'User-Agent' : generate_user_agent(os='win', device_type='desktop')}
+                                url = f'https://www.10000recipe.com/recipe/list.html?q=&query=&cat1={catM.index}&cat2={catS.index}&cat3={catI.index}&cat4={catT.index}&order=date&page={page}'
+                                res = requests.get(url, timeout=5, headers=headers)
+                                soup = BeautifulSoup(res.text, 'html.parser')
+                                items = soup.select('#contents_area_full ul ul li.common_sp_list_li div.common_sp_thumb a')
+                                
+                            for item in items:
+                                recipe_link = item.get('href').strip()
+                                recipe_link = recipe_link.split('/')[-1]
+                                recipe = Recipe.objects.filter(mangaeId__iexact=recipe_link)
+                                # print(recipe_link)
+                                # print(Recipe.objects.get(mangaeId='6843136').name)
+                                data['update_keys'].append(
+                                {
+                                    'mangaeId' : recipe_link,
+                                    'categoryTId' : catT.pk,
+                                    'categorySId' : catS.pk,
+                                    'categoryIId' : catI.pk,
+                                    'categoryMId' : catM.pk,
+                                })
+                                if recipe:
+                                    recipe.update(categoryTId=catT,categorySId=catS, categoryIId=catI, categoryMId=catM)
+                            page += 1
+                        except:
+                            print("connection aborted by the server ..")
+                            time.sleep(5)
+                            print("====restart====")
+                            continue
+                    print(f'category M : {catM.index} 종료')
                 print(f'category I : {catI.index} 종료')
-                break
-    #       print(f'category S : {catS.index} 종료')
-            break
-        # with open(file_path, 'w') as outfile:
-        #     json.dump(data, outfile)
-    #   print(f'category T : {catT.index} 종료')
-        break
+            print(f'category S : {catS.index} 종료')
+        print(f'category T : {catT.index} 종료')
+        with open(os.path.abspath(f'./scripts/jsons/catT{catT.pk}.json'),'w', encoding='utf-8') as f:
+            json.dump(data, f, ensure_ascii=False, indent=4)
 
 
 
