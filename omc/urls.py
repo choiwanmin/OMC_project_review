0493b8cdc1c5a43987be07ab519837af70bd986e
khj <rlaguswl706926@naver.com>
2022-12-23 10:04:14 +0900

    feat: refrigerator list view 템플릿 완료, views.py 수정

--- omc/urls.py
+++ omc/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name ="omc"
 urlpatterns = [
-    path('', views.Recipe_list.as_view(), name='recipe_view'),
-    path('detail/', views.Recipe_detail.as_view(), name='recipe_detail_view')
+    path('', views.RecipeList.as_view(), name='recipe_list'),
+    path('detail/', views.Recipe_detail.as_view(), name='recipe_detail_view'),
+    path('refrigerator/', views.Refrigerator_list.as_view(), name='refrigerator_view'),
 ]
--- omc/views.py
+++ omc/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import ListView, TemplateView
-from .models import models
+from django.views.generic import ListView, DetailView, TemplateView
+from .models import Recipe
 
 # Create your views here.
 def index(requests):
     # recipe = recipe.objects.all().order_by("-")
     return render(requests,"index.html")
 
-class Recipe_list(TemplateView):
-    template_name = 'omc/recipe.html'
 
-class Recipe_detail(TemplateView):
-    template_name = 'omc/recipe_detail.html'
+class RecipeList(ListView):
+    model = Recipe
+    paginate_by = 40
+
+class Recipe_detail(DetailView):
+	model = Recipe
+
+class Refrigerator_list(TemplateView):
+    template_name = 'omc/refrigerator_list.html'
