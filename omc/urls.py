f278c6ea33d2e9b8ba13824292d37c9fcb4cdb6b
chomu97 <ach1219@naver.com>
2022-12-23 10:07:51 +0900

    fix: name 수정

--- omc/urls.py
+++ omc/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 app_name ="omc"
 urlpatterns = [
-    path('', views.RecipeList.as_view(), name='recipe_list'),
+    path('', views.RecipeList.as_view(), name='recipe_list_view'),
     path('detail/', views.Recipe_detail.as_view(), name='recipe_detail_view'),
     path('refrigerator/', views.Refrigerator_list.as_view(), name='refrigerator_view'),
 ]
