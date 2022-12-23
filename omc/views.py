9b66cd9ebd74d90b873de27ccdf3280d0ec8dd01
lks <chgml9326@hanyang.ac.kr>
2022-12-23 18:42:18 +0900

    recipe list 페이지 구현

--- omc/views.py
+++ omc/views.py
@@ -11,6 +11,28 @@ def index(requests):
 class RecipeList(ListView):
     model = Recipe
     paginate_by = 40
+    template_name = 'omc/recipe_list_view.html'
+    ordering='pk'
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeList, self).get_context_data(**kwargs)
+        if not context.get('is_paginated', False):
+            return context
+
+        paginator = context.get('paginator')
+        num_pages = paginator.num_pages
+        current_page = context.get('page_obj')
+        page_no = current_page.number
+
+        if num_pages <= 11 or page_no <= 6:  # case 1 and 2
+            pages = [x for x in range(1, min(num_pages + 1, 12))]
+        elif page_no > num_pages - 6:  # case 4
+            pages = [x for x in range(num_pages - 10, num_pages + 1)]
+        else:  # case 3
+            pages = [x for x in range(page_no - 5, page_no + 6)]
+
+        context.update({'pages': pages})
+        return context
 
 class Recipe_detail(DetailView):
 	model = Recipe
