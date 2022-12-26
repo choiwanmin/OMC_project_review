e32ba3e5e7d5633c86425aea5dd1341b5149d5f9
lks <chgml9326@hanyang.ac.kr>
2022-12-26 20:39:54 +0900

    feat: 레시피 리스트 페이지에서 카테고리 nav 보이도록 구현 및 레시피 이름으로 검색 기능 추가

--- omc/urls.py
+++ omc/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.RecipeList.as_view(), name='recipe_list_view'),
     path('detail/', views.Recipe_detail.as_view(), name='recipe_detail_view'),
     path('refrigerator/', views.Refrigerator_list.as_view(), name='refrigerator_view'),
+    path('search/<str:q>/', views.RecipeSearch.as_view(), name='recipe_search'),
 ]
--- omc/views.py
+++ omc/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView, TemplateView
-from .models import Recipe
+from .models import Recipe, CategoryT, CategoryS, CategoryI, CategoryM
+from django.db.models import Q
+# from .forms import CategoryForm
 
 # Create your views here.
 def index(requests):
@@ -16,6 +18,7 @@ class RecipeList(ListView):
 
     def get_context_data(self, **kwargs):
         context = super(RecipeList, self).get_context_data(**kwargs)
+
         if not context.get('is_paginated', False):
             return context
 
@@ -32,6 +35,12 @@ class RecipeList(ListView):
             pages = [x for x in range(page_no - 5, page_no + 6)]
 
         context.update({'pages': pages})
+
+        context['category_t'] = CategoryT.objects.all()
+        context['category_s'] = CategoryS.objects.all()
+        context['category_i'] = CategoryI.objects.all()
+        context['category_m'] = CategoryM.objects.all()
+
         return context
 
 class Recipe_detail(DetailView):
@@ -39,3 +48,43 @@ class Recipe_detail(DetailView):
 
 class Refrigerator_list(TemplateView):
     template_name = 'omc/refrigerator_list.html'
+
+class RecipeSearch(RecipeList):
+    paginate_by = 40
+    # paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        recipe_list = Recipe.objects.filter(
+            Q(name__contains=q) # | Q(tags__name__contains=q) | Q(content__contains=q)
+        ).distinct()
+        return recipe_list
+
+    def get_context_data(self, **kwargs):
+        context = super(RecipeSearch, self).get_context_data(**kwargs)
+
+        if not context.get('is_paginated', False):
+            pages = [1]
+        else:
+            paginator = context.get('paginator')
+            num_pages = paginator.num_pages
+            current_page = context.get('page_obj')
+            page_no = current_page.number
+
+            if num_pages <= 11 or page_no <= 6:  # case 1 and 2
+                pages = [x for x in range(1, min(num_pages + 1, 12))]
+            elif page_no > num_pages - 6:  # case 4
+                pages = [x for x in range(num_pages - 10, num_pages + 1)]
+            else:  # case 3
+                pages = [x for x in range(page_no - 5, page_no + 6)]
+            
+        context.update({'pages': pages})
+        context['category_t'] = CategoryT.objects.all()
+        context['category_s'] = CategoryS.objects.all()
+        context['category_i'] = CategoryI.objects.all()
+        context['category_m'] = CategoryM.objects.all()
+        
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+        context['search_word'] = q
+        return context
