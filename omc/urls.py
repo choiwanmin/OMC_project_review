88057eb4d1e6fa1d4aa1be882fa8deb99ff7f522
chomu97 <ach1219@naver.com>
2023-01-11 19:01:42 +0900

    fix: name 수정

--- omc/urls.py
+++ omc/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('<int:pk>/', views.RecipeDetail.as_view(), name='recipe_detail_view'),
     path('refrigerator/', views.RefrigeratorList.as_view(), name='refrigerator_view'),
     path('search/<str:q>/', views.RecipeSearch.as_view(), name='recipe_search'),
-    re_path(r'category/[0-9]{8}/', views.RecipeCategory.as_view(), name='recipe_search'),
+    re_path(r'category/[0-9]{8}/', views.RecipeCategory.as_view(), name='recipe_category'),
 ]
  
