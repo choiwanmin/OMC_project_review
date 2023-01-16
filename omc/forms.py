4b76f78bcf2dbb273a13c1153b7e3e5ee952421e
lks <chgml9326@hanyang.ac.kr>
2023-01-16 20:01:21 +0900

    feat: S3 연결 1차

--- omc/forms.py
+++ omc/forms.py
@@ -1,7 +1,26 @@
 from django import forms
 from .models import Comment
+from django.core.validators import validate_image_file_extension
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('content','star','thumbnail',)
+        widgets = {
+            'content' : forms.TextInput(
+                attrs={
+                    'class' : 'form-control',
+                    # 'placeholder': 'content'
+                }
+            ),
+            'star' : forms.NumberInput(
+                attrs={
+                    'class' : 'form-control',
+                    'max' : '5',
+                    'min' : '0',
+                }
+            ),
+            'thumbnail' : forms.ImageField(
+                validators=[validate_image_file_extension],
+            ),
+        }
