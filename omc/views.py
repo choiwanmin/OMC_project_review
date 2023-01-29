f18a37e2a8ce313f646d47d884b27652e17ee04e
choiwanmin <choi.wm512@gmail.com>
2023-01-29 20:20:08 +0900

    feat: 댓글 기능 구현 수정(댓글 수정 기능 진행중)

--- omc/views.py
+++ omc/views.py
@@ -248,7 +248,6 @@ class UpdateComment(LoginRequiredMixin, UpdateView):
         else:
             raise PermissionDenied
 
-
 def delete_comment(request,pk):
     comment = get_object_or_404(Comment, pk=pk)
     recipe = comment.recipeId
